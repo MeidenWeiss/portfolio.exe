6eca31e13b24268cf4bb2c14495377ad22d28eac
Protein Dispenser <60122610+MeidenWeiss@users.noreply.github.com>
2020-11-25 13:38:56 +0800

    Add files via upload

---
+++ js/amd.js
@@ -0,0 +1,39 @@
+var ctx = document.getElementById("amd").getContext("2d");
+
+ctx.save();
+ctx.transform(1.000000, 0.000000, 0.000000, 1.000000, -1.127530, -1.663090);
+	
+
+ctx.beginPath();
+ctx.fillStyle = 'rgb(0, 0, 0)';
+ctx.lineWidth = 0.122110;
+ctx.moveTo(1.394798, 44.038543);
+ctx.lineTo(1.394798, 33.533582);
+ctx.lineTo(8.779098, 26.160100);
+ctx.lineTo(16.163400, 18.786618);
+ctx.lineTo(16.163400, 29.050002);
+ctx.lineTo(16.163400, 39.313385);
+ctx.lineTo(26.239891, 39.313385);
+ctx.lineTo(36.316386, 39.313385);
+ctx.lineTo(36.640659, 39.637657);
+ctx.lineTo(36.964930, 39.961930);
+ctx.lineTo(29.684827, 47.252717);
+ctx.lineTo(22.404721, 54.543503);
+ctx.lineTo(11.899760, 54.543503);
+ctx.lineTo(1.394798, 54.543503);
+ctx.moveTo(46.502848, 46.807817);
+ctx.lineTo(39.239337, 39.533650);
+ctx.lineTo(39.239337, 27.885548);
+ctx.lineTo(39.239337, 16.237446);
+ctx.lineTo(27.580403, 16.237446);
+ctx.lineTo(15.921471, 16.237446);
+ctx.lineTo(8.779098, 9.083905);
+ctx.lineTo(1.636727, 1.930363);
+ctx.lineTo(27.822332, 1.930363);
+ctx.lineTo(54.007937, 1.930363);
+ctx.lineTo(54.007937, 28.006175);
+ctx.lineTo(54.007937, 54.081985);
+ctx.lineTo(53.887143, 54.081985);
+ctx.lineTo(53.766348, 54.081985);
+ctx.fill();
+ctx.restore();
---
+++ js/mastercard.js
@@ -0,0 +1,25 @@
+var canvas = document.getElementById("card");
+var context = canvas.getContext("2d");
+
+context.save();
+context.beginPath();
+context.moveTo(44, 81.5 - 32.5);
+context.bezierCurveTo(44 + (0.5522847498307936 * 32), 81.5 - 32.5,  44 + 32, 81.5 - (0.5522847498307936 * 32.5), 44 + 32, 81.5);
+context.bezierCurveTo(44 + 32, 81.5 + (0.5522847498307936 * 32.5), 44 + (0.5522847498307936 * 32), 81.5 + 32.5, 44, 81.5 + 32.5);
+context.bezierCurveTo(44 - (0.5522847498307936 * 32), 81.5 + 32.5, 44 - 32, 81.5 + (0.5522847498307936 * 32.5), 44 - 32, 81.5);
+context.bezierCurveTo(44 - 32, 81.5 - (0.5522847498307936 * 32.5), 44 - (0.5522847498307936 * 32), 81.5 - 32.5, 44, 81.5 - 32.5);
+context.fillStyle = "red";
+context.fill();
+context.closePath();
+
+context.save();
+context.beginPath();
+context.moveTo(90, 81.5 - 32.5);
+context.bezierCurveTo(90 + (0.5522847498307936 * 32), 81.5 - 32.5,  90 + 32, 81.5 - (0.5522847498307936 * 32.5), 90 + 32, 81.5);
+context.bezierCurveTo(90 + 32, 81.5 + (0.5522847498307936 * 32.5), 90 + (0.5522847498307936 * 32), 81.5 + 32.5, 90, 81.5 + 32.5);
+context.bezierCurveTo(90 - (0.5522847498307936 * 32), 81.5 + 32.5, 90 - 32, 81.5 + (0.5522847498307936 * 32.5), 90 - 32, 81.5);
+context.bezierCurveTo(90 - 32, 81.5 - (0.5522847498307936 * 32.5), 90 - (0.5522847498307936 * 32), 81.5 - 32.5, 90, 81.5 - 32.5);
+context.globalAlpha = 0.8;
+context.fillStyle = "rgb(255, 204, 0)";
+context.fill();
+context.closePath();
